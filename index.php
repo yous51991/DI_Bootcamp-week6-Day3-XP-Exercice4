f953ee073dd1a7d2d27462fef5976b46b46b92f9
Youssouf OUATTARA <121155997+yous51991@users.noreply.github.com>
2023-02-01 16:08:11 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="digit.php" method="post">
+        <div class="input-group mb-3">
+            <span>Entrez des nombres en lettre separes par des virgules: </span>
+            <input type="text   " class="form-control" name="valeur" />
+        </div>
+        <input type="submit" />
+    </form>
+
+
+
+
+</body>
+
+</html>
